969b268a57584d0afaa1c8f90d0be5c17e226e93
hadil <had33l-@outlook.com>
2025-09-04 17:20:35 +0000

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio/' // <-- tells Vite to use repo name as base path
 })
